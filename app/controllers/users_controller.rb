1a4accd1487425502c828df286ef9747fe09c477
Daron Adkins <daron@kerigan.com>
2016-06-08 11:29:49 -0500

    Change flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Please check your email to activate your account. Allow 1 - 5 minutes for the email to be delivered."
       redirect_to root_url
     else
       render 'new'
