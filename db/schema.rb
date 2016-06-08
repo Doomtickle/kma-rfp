e65c8a4213f541eebb57d0d0a6e16a63f6fc32de
Daron Adkins <daron@kerigan.com>
2016-06-08 10:41:00 -0500

    Add company name to users db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607233741) do
+ActiveRecord::Schema.define(version: 20160608153038) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20160607233741) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "company_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
