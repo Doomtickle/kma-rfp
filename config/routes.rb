b1bee2b3f4bd783a2381cab274a07a5142493380
Daron Adkins <daron@kerigan.com>
2016-06-21 13:40:13 -0500

    Change routes to display proposal request#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :proposal_requests
   root                'static_pages#home'
-  get    'help'    => 'static_pages#help'
-  get    'about'   => 'static_pages#about'
-  get    'contact' => 'static_pages#contact'
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get    'help'                => 'static_pages#help'
+  get    'about'               => 'static_pages#about'
+  get    'contact'             => 'static_pages#contact'
+  get    'signup'              => 'users#new'
+  get    'login'               => 'sessions#new'
+  get    'new-proposal'        => 'proposal_requests#new'
+  post   'login'               => 'sessions#create'
+  delete 'logout'              => 'sessions#destroy'
   resources :users do
     member do
       get :following, :followers
@@ -17,4 +18,5 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
+  resources :microposts,          only: [:new, :create, :edit, :update]
 end
