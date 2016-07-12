52dabbbd90902181f5764155f3442087bc9cdc24
Daron Adkins <daron@kerigan.com>
2016-07-12 14:53:07 -0500

    Yo

--- app/controllers/proposal_requests_controller.rb
+++ app/controllers/proposal_requests_controller.rb
@@ -30,7 +30,7 @@ class ProposalRequestsController < ApplicationController
 
       if @proposal_request.save
         flash[:success] =  'Proposal request was successfully created.' 
-        redirect_to ProposalRequest.show 
+        redirect_to @proposal_request 
       else
           redirect_to 'static_pages/home'
       end
