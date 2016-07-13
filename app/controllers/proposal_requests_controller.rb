420bccc8db9769b13e34e1b9427e8f6be0debd60
Daron Adkins <daron@kerigan.com>
2016-07-13 08:40:47 -0500

    Hey there

--- app/controllers/proposal_requests_controller.rb
+++ app/controllers/proposal_requests_controller.rb
@@ -12,6 +12,7 @@ class ProposalRequestsController < ApplicationController
   # GET /proposal_requests/1
   # GET /proposal_requests/1.json
   def show
+    @proposal_request = ProposalRequest.find(params[:id])
       respond_to do |format|
         format.html
         format.csv { send_data @proposal_request.to_csv }
@@ -73,7 +74,7 @@ class ProposalRequestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def proposal_request_params
-      params.require(:proposal_request).permit(:user_id, :client_name, :client_industry, :campaign_name, :basic_description, :flight_date_start, :flight_date_end, :staggered, :goals_and_objectives, :budget, :targeting1, :targeting2, :targeting3, :targeting4, :geography, :age_group, :gender, :household_income, :interests, :devices, :creative_ad_units, :day_parting, :success_metrics, :specifications, :insertion_order_terms, :proposal_due_dates, :decision_made_by, :selection_criteria)
+      params.require(:proposal_request).permit(:client_name, :client_industry, :campaign_name, :basic_description, :flight_date_start, :flight_date_end, :staggered, :goals_and_objectives, :budget, :targeting1, :targeting2, :targeting3, :targeting4, :geography, :age_group, :gender, :household_income, :interests, :devices, :creative_ad_units, :day_parting, :success_metrics, :specifications, :insertion_order_terms, :proposal_due_dates, :decision_made_by, :selection_criteria)
     end
 
     def correct_user
