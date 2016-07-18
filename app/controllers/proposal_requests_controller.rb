ad9bbad5e33f08262f3ef14be78ebf5e0d027269
Daron Adkins <daron@kerigan.com>
2016-07-18 08:37:34 -0500

    Fix more plurals

--- app/controllers/proposal_requests_controller.rb
+++ app/controllers/proposal_requests_controller.rb
@@ -69,6 +69,6 @@ class ProposalRequestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def proposal_request_params
-      params.require(:proposal_request).permit(:client_name, :client_industry, :campaign_name, :basic_description, :flight_date_start, :flight_date_end, :staggered, :goals_and_objectives, :budget, :targeting1, :geography, :age_group, :gender, :household_income, :interests, :devices, :device_description, :creative_ad_units, :day_parting, :day_parting_description, :success_metrics, :specifications, :insertion_order_terms, :submission_instructions, :acceptable_proposal_formats, :proposal_due_date, :decision_made_by, :selection_criteria)
+      params.require(:proposal_request).permit(:client_name, :client_industry, :campaign_name, :basic_description, :flight_date_start, :flight_date_end, :staggered, :goals_and_objectives, :budget, :targeting1, :geography, :age_group, :gender, :household_income, :interests, :devices, :devices_description, :creative_ad_units, :day_parting, :day_parting_description, :success_metrics, :specifications, :insertion_order_terms, :submission_instructions, :acceptable_proposal_formats, :proposal_due_date, :decision_made_by, :selection_criteria)
     end
 end
