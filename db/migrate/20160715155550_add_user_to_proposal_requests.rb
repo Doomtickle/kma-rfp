36aedb7e65ac18b87e003fef8f0cbc2be85a6579
Daron Adkins <daron@kerigan.com>
2016-07-18 09:55:50 -0500

    No i didn't

--- db/migrate/20160715155550_add_user_to_proposal_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToProposalRequests < ActiveRecord::Migration
-  def change
-    add_reference :proposal_requests, :user, index: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -24,42 +24,6 @@ ActiveRecord::Schema.define(version: 20160715155550) do
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
-  create_table "proposal_requests", force: :cascade do |t|
-    t.string   "client_name"
-    t.string   "client_industry"
-    t.string   "campaign_name"
-    t.text     "basic_description"
-    t.date     "flight_date_start"
-    t.date     "flight_date_end"
-    t.string   "staggered"
-    t.text     "goals_and_objectives"
-    t.float    "budget"
-    t.string   "targeting"
-    t.string   "geography"
-    t.string   "age_group"
-    t.string   "gender"
-    t.string   "household_income"
-    t.string   "interests"
-    t.string   "devices"
-    t.string   "devices_description"
-    t.text     "creative_ad_units"
-    t.string   "day_parting"
-    t.string   "day_parting_description"
-    t.string   "success_metrics"
-    t.text     "specifications"
-    t.text     "insertion_order_terms"
-    t.text     "submission_instructions"
-    t.string   "acceptable_proposal_formats"
-    t.date     "proposal_due_date"
-    t.date     "decision_made_by"
-    t.text     "selection_criteria"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "user_id"
-  end
-
-  add_index "proposal_requests", ["user_id"], name: "index_proposal_requests_on_user_id"
-
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
