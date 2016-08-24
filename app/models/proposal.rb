1272f1c0c93893f373c0646e7acc8d4eee43a019
Daron Adkins <daron@kerigan.com>
2016-08-24 13:37:15 -0500

    Testing Validation

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,3 +1,7 @@
 class Proposal < ActiveRecord::Base
   belongs_to :user
+    validates :rfp_amount, 
+      :presence => true,
+      :format => { :with => /^[0-9]/, :message => 'Please only enter numbers'},
+      :on => :create
 end
