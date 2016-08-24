a1a652587629b0d0c285cfc5f23927959776e1f0
Daron Adkins <daron@kerigan.com>
2016-08-24 13:54:55 -0500

    more valitate

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,6 +1,6 @@
 class Proposal < ActiveRecord::Base
   belongs_to :user
-    validates :rfp_amount, 
+    validates :rfp_amount, :purchased_60, :purchased_30, :cpps, 
       :format => { :with => /[0-9]/, :message => 'Please only enter numbers'},
       :on => :create
 end
